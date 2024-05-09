8fdbe067df71ab4d93a19108f46fce0440033301
juanvelasco1 <131167099+juanvelasco1@users.noreply.github.com>
2024-05-08 19:21:27 -0500

    fix

--- src/components/card/card.ts
+++ src/components/card/card.ts
@@ -1,55 +1,55 @@
-import {loadCss} from '../../utils/styles';
+import { loadCss } from '../../utils/styles';
 import stylesCard from '../card/card.css';
 
 export enum Attribute {
-    'photo' = 'photo',
-    'name' = 'name',
-    'image' = 'image',
-    'tag' = 'tag',
-    'description' = 'description',
+	'photo' = 'photo',
+	'name' = 'name',
+	'image' = 'image',
+	'tag' = 'tag',
+	'description' = 'description',
 }
 
 class Card extends HTMLElement {
-    photo?: string;
-    name?: string;
-    image?: string;
-    description?: string;
-    tag?: string;
+	photo?: string;
+	name?: string;
+	image?: string;
+	description?: string;
+	tag?: string;
 
-    constructor() {
-        super();
-        this.attachShadow({mode: 'open'});
-    }
+	constructor() {
+		super();
+		this.attachShadow({ mode: 'open' });
+	}
 
-    static get observedAttributes() {
-        const attrs: Record<Attribute, null> = {
-            photo: null,
-            name: null,
-            image: null,
-            description: null,
-            tag: null,
-        };
+	static get observedAttributes() {
+		const attrs: Record<Attribute, null> = {
+			photo: null,
+			name: null,
+			image: null,
+			description: null,
+			tag: null,
+		};
 
-        return Object.keys(attrs);
-    }
+		return Object.keys(attrs);
+	}
 
-    attributeChangedCallback(propName: Attribute, oldValue: string | undefined, newValue: string | undefined) {
-        switch (propName) {
-            default:
-                this[propName] = newValue;
-                break;
-        }
-        this.render();
-    }
+	attributeChangedCallback(propName: Attribute, oldValue: string | undefined, newValue: string | undefined) {
+		switch (propName) {
+			default:
+				this[propName] = newValue;
+				break;
+		}
+		this.render();
+	}
 
-    connectedCallback() {
-        this.render();
-    }
+	connectedCallback() {
+		this.render();
+	}
 
-    render() {
-        if (this.shadowRoot) {
-            loadCss(this, stylesCard);
-            this.shadowRoot.innerHTML = `
+	render() {
+		if (this.shadowRoot) {
+			loadCss(this, stylesCard);
+			this.shadowRoot.innerHTML = `
 
 			<style>
 			${stylesCard}
@@ -68,7 +68,7 @@ class Card extends HTMLElement {
                         <button class="heart" type="button">
                             <img class="like" id="likeButton" src="/src/asset/like.png" alt="Like">
                         </button>
-                    
+
                         <img class="comment" src="/src/asset/comment.png" alt="Comment">
                         <div class="tag">
                             <p><strong>${this.tag}</strong></p>
@@ -77,52 +77,52 @@ class Card extends HTMLElement {
                             <img class="save" id="saveButton" src="/src/asset/save.png" alt="Save">
                         </button>
                     </section>
-                    
+
                     <section class="desc-section ">
                         <p class="desc">${this.description}</p>
                     </section>
-   
-                    
+
+
                 </main>
             </div>
       `;
-        }
-        const cssCard = this.ownerDocument.createElement('style');
-        cssCard.innerHTML = stylesCard;
-        this.shadowRoot?.appendChild(cssCard);
+		}
+		const cssCard = this.ownerDocument.createElement('style');
+		cssCard.innerHTML = stylesCard;
+		this.shadowRoot?.appendChild(cssCard);
 
-        const changeButton = this.shadowRoot?.getElementById('likeButton');
+		const changeButton = this.shadowRoot?.getElementById('likeButton');
 
-        changeButton?.addEventListener('click', function () {
-            if (changeButton.getAttribute('src') === '/src/asset/like.png') {
-                changeButton.setAttribute('src', '/src/asset/like-full.png');
-            } else {
-                changeButton.setAttribute('src', '/src/asset/like.png');
-            }
-        });
+		changeButton?.addEventListener('click', function () {
+			if (changeButton.getAttribute('src') === '/src/asset/like.png') {
+				changeButton.setAttribute('src', '/src/asset/like-full.png');
+			} else {
+				changeButton.setAttribute('src', '/src/asset/like.png');
+			}
+		});
 
-        const changeButtonSave = this.shadowRoot?.getElementById('saveButton');
+		const changeButtonSave = this.shadowRoot?.getElementById('saveButton');
 
-        changeButtonSave?.addEventListener('click', function () {
-            if (changeButtonSave.getAttribute('src') === '/src/asset/save.png') {
-                changeButtonSave.setAttribute('src', '/src/asset/save-full.png');
-            } else {
-                changeButtonSave.setAttribute('src', '/src/asset/save.png');
-            }
-        });
+		changeButtonSave?.addEventListener('click', function () {
+			if (changeButtonSave.getAttribute('src') === '/src/asset/save.png') {
+				changeButtonSave.setAttribute('src', '/src/asset/save-full.png');
+			} else {
+				changeButtonSave.setAttribute('src', '/src/asset/save.png');
+			}
+		});
 
-        // const like = document.getElementById('like');
-        // const image1 = document.getElementById('image1');
-        // const image2 = document.getElementById('image2');
-        // const likeF = document.getElementById('likeF');
+		// const like = document.getElementById('like');
+		// const image1 = document.getElementById('image1');
+		// const image2 = document.getElementById('image2');
+		// const likeF = document.getElementById('likeF');
 
-        // if (like && image1 && image2 && likeF) {
-        // 	like.style.display = image1.style.display === 'none' ? 'block' : 'none';
-        // 	likeF.style.display = image2.style.display === 'none' ? 'block' : 'none';
-        // }
-    }
+		// if (like && image1 && image2 && likeF) {
+		// 	like.style.display = image1.style.display === 'none' ? 'block' : 'none';
+		// 	likeF.style.display = image2.style.display === 'none' ? 'block' : 'none';
+		// }
+	}
 
-    /*	const photo = document.createElement('img');
+	/*	const photo = document.createElement('img');
     photo.innerHTML = img;
     this.shadowRoot?.appendChild(photo);
 
