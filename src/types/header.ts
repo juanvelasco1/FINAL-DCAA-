0eca03dd34ccc5843edefac0d0efe1c488809f94
juanvelasco1 <131167099+juanvelasco1@users.noreply.github.com>
2024-05-08 19:55:26 -0500

    feat:types

---
+++ src/types/header.ts
@@ -0,0 +1,6 @@
+export interface headerTypes {
+	logo: string;
+	notification: string;
+	photo: string;
+	type: string;
+}
---
+++ src/types/sections.ts
@@ -0,0 +1,4 @@
+export interface sectionsTypes {
+	image: string;
+	type: string;
+}
